4e10b9f124e9b8aead699cfb40fc5fe93289951d
Erik <post@erikroed.no>
2025-08-21 21:02:26 +0000

    Update plugins/treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    branch = "main",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter").install { 
-            'lua', 'vim', 'bash', 'awk', 'csv', 'diff', 'dockerfile', 'dot', 'gitignore', 
-            'go', 'gomod', 'gosum', 'gotmpl', 'helm', 'jq', 'json', 'markdown', 'python', 'regex', 
-            'ssh_config', 'terraform', 'tmux', 'yaml' 
-        }
-
-        vim.api.nvim_create_autocmd('FileType', {
-            pattern = { '<filetype>' },
-            callback = function() vim.treesitter.start() end,
-        })
-    end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "lua", "vim", "go" },
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
+	},
 }
-
