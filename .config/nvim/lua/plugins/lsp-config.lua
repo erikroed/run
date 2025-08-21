565016d253edc8095d490982460d0f74c33f3b52
Erik <post@erikroed.no>
2025-08-21 19:28:49 +0000

    Update lsp-config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -13,27 +13,20 @@ return {
         },
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "ts_ls" }
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            vim.lsp.config['luals'] = {
-                cmd = { 'lua-language-server' },
-                filetypes = { 'lua' },
-                root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                        }
-                    }
-                }
-            }
-            vim.lsp.enable("luals")
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover(), {})
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.ts_ls.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
