7073379343ab00cf5ff1703291f732306f4b06fd
Erik <post@erikroed.no>
2025-09-01 17:13:04 +0000

    Update config for treesitter plugin

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,17 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "vim", "go" },
-				auto_install = true,
-				highlight = {
-					enable = true,
-				},
-			})
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        branch = "main",
+        lazy = false,
+        config = function()
+            require("nvim-treesitter").install({
+                ensure_installed = { "lua", "vim", "go" },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+            })
+        end,
+    },
 }
